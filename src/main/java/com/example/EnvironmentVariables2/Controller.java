bc9903bb0a8e45262ef3d9a92520c57fdc7acf6f
Fabrizio Lagioia <meandi958@gmail.com>
2024-06-09 18:56:04 +0200

    environmentVariables2

---
+++ src/main/java/com/example/EnvironmentVariables2/Controller.java
@@ -0,0 +1,18 @@
+package com.example.EnvironmentVariables2;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @Autowired
+    Environment environment;
+
+    @GetMapping
+    public String heyThere () {
+        return environment.getProperty("welcomeMsg");
+    }
+}
